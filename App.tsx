870f017decac2cd3a97bf7810da2648b973bb91e
Farouq Seriki <farouq@basaltx.io>
2024-07-01 05:07:24 +0100

    Conditional rendering StoryBook done

--- App.tsx
+++ App.tsx
@@ -7,52 +7,14 @@ import {CompetitionList} from './components/CompetitionList';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigations/StackNavigation';
 
-const storyBookApp = 'false';
+// CHANGE HERE
+const storyBookApp = 'true';
+// CHANGE HERE
 
 function App(): React.JSX.Element {
   const [vals, setVals] = useState('dd');
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      {/* <Text>{vals}</Text> */}
-      {/* <MyButton
-        text="Fas Button"
-        onPress={() => console.log('Fas says hello')}
-      /> */}
-      {/* <AppButtons
-        onPress={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-        text={'Sign up for free'}
-        title={'GO_BACK'}
-      /> */}
-      {/* <InputField
-        onPress={() => console.log('pressed')}
-        text={''}
-        title={'PASSWORD_INPUT'}
-        placeholder="Testing"
-        showPassword={true}
-        value={vals}
-        onChangeText={(text: string) => setVals(text)}
-      /> */}
-      {/* <CompetitionList
-        comapetition={[
-          {
-            title: '20th Asian Game - Achi Nagoya 2026 (Winter)',
-            fromDate: '2000-12-02',
-            toDate: '2024-14-02',
-            location: 'Pyeongchang, Gangwon-do, Korea Very Very long city name',
-            id: '1',
-          },
-          {
-            title: 'Achi Nagoya 2026 (Winter)',
-            fromDate: '2000-12-02',
-            toDate: '2024-14-02',
-            location: 'Pyeongchang, Gangwon-do, Korea Very Very long city name',
-            id: '2',
-          },
-        ]}
-        onPress={() => console.log('')}
-      /> */}
       <NavigationContainer>
         <StackNavigation />
       </NavigationContainer>
